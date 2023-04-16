c2fd34c9128aacb18234f440d68695e2fde36109
nblasd <gameranonymous338@gmail.com>
2023-04-16 20:42:00 +0300

    Updated project with new features

--- components/BarChart.tsx
+++ components/BarChart.tsx
@@ -19,8 +19,19 @@ ChartJS.register(
   Legend
 );
 
+interface ChartData {
+  labels: string[],
+  datasets: {
+    label: string,
+    data: number[],
+    borderColor: string,
+    backgroundColor: string
+  }[]
+}
+
 const BarChart = () => {
-  const [chartData, setChartData] = useState({
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: [],
     datasets: [],
   });
 
@@ -35,7 +46,7 @@ const BarChart = () => {
                 data: [18127, 22201, 19490, 17938, 24182, 17842, 22475],
                 borderColor: 'rgb(53, 162, 235)',
                 backgroundColor: 'rgb(53, 162, 235, 0.4',
-              }, 
+            }, 
         ]
     })
     setChartOptions({
